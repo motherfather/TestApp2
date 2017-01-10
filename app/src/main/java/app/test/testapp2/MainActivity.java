8e6c9b1f8431112b8f3104fe16bd52d63493032d
motherfather <father@mail.com>
2017-01-10 21:27:51 +0900

    값 보내는게 안돼 ㅠㅠ

--- app/src/main/java/app/test/testapp2/MainActivity.java
+++ app/src/main/java/app/test/testapp2/MainActivity.java
@@ -1,18 +1,23 @@
 package app.test.testapp2;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     static String str = "";
     Button button;
     TextView textView;
+    EditText editName;
+    EditText editCountry;
+    EditText editTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +36,17 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button)findViewById(R.id.accept);
         textView = (TextView)findViewById(R.id.textView);
+        editName = (EditText)findViewById(R.id.editName);
+        editCountry = (EditText)findViewById(R.id.editCountry);
+        editTwitter = (EditText)findViewById(R.id.editTwitter);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 HttpAsyncTask httpAsyncTask = new HttpAsyncTask(MainActivity.this);
-                httpAsyncTask.execute("http://hmkcode.appspot.com/jsonservlet");
+                httpAsyncTask.execute("http://hmkcode.appspot.com/jsonservlet", editName.getText().toString(), editCountry.getText().toString(), editTwitter.getText().toString());
             }
         });
-
     }
 
     private class HttpAsyncTask extends AsyncTask<String, Void, String> {
@@ -49,15 +59,32 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
+            Person person = new Person();
+            person.setName(strings[1]);
+            person.setCountry(strings[2]);
+            person.setTwitter(strings[3]);
+
             try {
                 URL url = new URL(strings[0]);
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
 
-                httpURLConnection.setRequestProperty("content-type", "application/json");
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("name", person.getName());
+                jsonObject.accumulate("country", person.getCountry());
+                jsonObject.accumulate("twitter", person.getTwitter());
 
+                httpURLConnection.setRequestProperty("content-type", "application/json");
+                httpURLConnection.setRequestProperty("accept", "application/json");
+
+                httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
 
                 InputStream is = httpURLConnection.getInputStream();
+                OutputStream os = httpURLConnection.getOutputStream();
+
+                String json = jsonObject.toString();
+                os.write(json.getBytes());
+                os.flush();
 
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
                 String line = "";
@@ -75,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             str = result;
             mainActivity.runOnUiThread(new Runnable() {
-
                 @Override
                 public void run() {
                     try {
@@ -86,6 +112,45 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+        }
+    }
+
+    private class Person {
+        private String name;
+        private String country;
+        private String twitter;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getTwitter() {
+            return twitter;
+        }
+
+        public void setTwitter(String twitter) {
+            this.twitter = twitter;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", country='" + country + '\'' +
+                    ", twitter='" + twitter + '\'' +
+                    '}';
         }
     }
 }
